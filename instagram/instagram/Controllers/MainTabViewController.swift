728d15591a67062bf8dcaa12848b3907eb7717e3
Roy-wonji <shuwj81@daum.net>
2022-04-27 02:43:36 +0900

    ✨[feat]: tab bar에  이미지 추가 함수 구현 및 tabbar color 변경  구현
    * tempateNavigationController tabbar 이미지를 지정 해주는 함수및  controller도 구현 해주는 함수  구현
    * configureViewControllers() 에 각 하나씩 탭바에 탭바   이미지랑  선택 되었을때  이미지 구현 및  연결되는 view 설정 구현

--- instagram/instagram/Controllers/MainTabViewController.swift
+++ instagram/instagram/Controllers/MainTabViewController.swift
@@ -10,24 +10,33 @@ import UIKit
 final class MainTabViewController:  UITabBarController {
     
     //MARK:  - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewControllers()
+        
     }
-
+    
     //MARK:  - Helpers
     
     func configureViewControllers() {
         view.backgroundColor = .white
-        
-        let feed = FeedController()
-        let search = SearchController()
-        let imageSelector = ImageSelectorController()
-        let notifications = NotificationController()
-        let profile = ProfileController()
+        let feed = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"),  selectedImage:  #imageLiteral(resourceName: "home_selected") , rootViewController: FeedController())
+        let search = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_selected"),  selectedImage:  #imageLiteral(resourceName: "search_selected") , rootViewController: SearchController())
+        let imageSelector = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"),  selectedImage:  #imageLiteral(resourceName: "plus_unselected") , rootViewController: ImageSelectorController())
+        let notifications = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"),  selectedImage:  #imageLiteral(resourceName: "like_selected") , rootViewController: NotificationController())
+        let profile = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"),  selectedImage:  #imageLiteral(resourceName: "profile_selected") , rootViewController: ProfileController())
         viewControllers = [feed , search, imageSelector, notifications, profile]
+        tabBar.tintColor = .black
+        tabBar.backgroundColor = .white
     }
-
+    
+    func tempateNavigationController(unselectedImage: UIImage, selectedImage: UIImage , rootViewController: UIViewController) -> UINavigationController {
+        let navigation = UINavigationController(rootViewController: rootViewController)
+        navigation.tabBarItem.image = unselectedImage
+        navigation.tabBarItem.selectedImage = selectedImage
+        navigation.navigationBar.tintColor = .black
+        return navigation
+    }
 }
 
