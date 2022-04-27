462bfacd4f1cb7615ed9a61d9a94bd89aa12bbc4
Roy-wonji <shuwj81@daum.net>
2022-04-27 22:53:08 +0900

    ✨[feat]: UICollectionViewController 로 구현
    * MainTabViewController에서  UICollectionViewFlowLayout 구현
    * FeedController에서 UICollectionViewDataSource 와 UICollectionViewDelegateFlowLayout  확장자 구현
    * collectionView에 forCellWithReuseIdentifier 구현
    * resueIdentifier  구현

---
+++ instagram/instagram/Controllers/TabBarController/FeedController.swift
@@ -0,0 +1,49 @@
+//
+//  FeedController.swift
+//  instagram
+//
+//  Created by 서원지 on 2022/04/27.
+//
+
+import UIKit
+
+private let resueIdentifier = "Cell"
+
+final class FeedController:  UICollectionViewController {
+    
+    //MARK:  - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - UI 설정 하는 함수
+
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: resueIdentifier)
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: resueIdentifier, for:  indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+//MARK:  - UICollectionViewDelegateFlowLayout
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+}
---
+++ instagram/instagram/Controllers/TabBarController/MainTabViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  instagram
+//
+//  Created by 로이 on 2022/04/27.
+//
+
+import UIKit
+
+final class MainTabViewController:  UITabBarController {
+    
+    //MARK:  - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewControllers()
+        
+    }
+    
+    //MARK:  - Helpers
+    
+   private func configureViewControllers() {
+        view.backgroundColor = .white
+    
+        let layout =  UICollectionViewFlowLayout( )
+        let feed = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"),  selectedImage:  #imageLiteral(resourceName: "home_selected")
+        ,rootViewController: FeedController(collectionViewLayout: layout))
+        let search = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_selected"),  selectedImage:  #imageLiteral(resourceName: "search_selected") , rootViewController: SearchController())
+        let imageSelector = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"),  selectedImage:  #imageLiteral(resourceName: "plus_unselected") , rootViewController: ImageSelectorController())
+        let notifications = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"),  selectedImage:  #imageLiteral(resourceName: "like_selected") , rootViewController: NotificationController())
+        let profile = tempateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"),  selectedImage:  #imageLiteral(resourceName: "profile_selected") , rootViewController: ProfileController())
+        viewControllers = [feed , search, imageSelector, notifications, profile]
+        tabBar.tintColor = .black
+        tabBar.backgroundColor = .white
+    }
+    //MARK: tabbar 의 이미지가 선택 되었을때랑 안선택 되었을때 이미지 선택 해주는 함수
+   private func tempateNavigationController(unselectedImage: UIImage, selectedImage: UIImage , rootViewController: UIViewController) -> UINavigationController {
+        let navigation = UINavigationController(rootViewController: rootViewController)
+        navigation.tabBarItem.image = unselectedImage
+        navigation.tabBarItem.selectedImage = selectedImage
+        navigation.navigationBar.tintColor = .black
+        return navigation
+    }
+}
+
