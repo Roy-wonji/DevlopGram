bf6ee74153c5a5b3c6c38a3976e85c86927369fe
Roy-wonji <shuwj81@daum.net>
2022-04-27 23:28:02 +0900

    ✨[feat]: uiimageview 에 대한  제약 조건  추가및  구현
    *  둥굴게 이미지뷰를 구현

--- instagram/instagram/VIew/TabBarCell/FeedCell.swift
+++ instagram/instagram/VIew/TabBarCell/FeedCell.swift
@@ -14,14 +14,26 @@ class FeedCell: UICollectionViewCell {
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.isUserInteractionEnabled = true
+        imageView.image = UIImage(named: "venom-7")
     }
     
     //MARK:  - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureUI()
         
-        backgroundColor = .systemPurple
+    }
+    
+    func configureUI() {
+        backgroundColor = .white
+        addSubview(profileImageView)
+        profileImageView.anchor(top : topAnchor,
+                                left: leftAnchor ,
+                                paddingTop: 12 ,
+                                paddingLeft: 12)
+        profileImageView.setDimensions(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
     }
     
     required init?(coder: NSCoder) {
