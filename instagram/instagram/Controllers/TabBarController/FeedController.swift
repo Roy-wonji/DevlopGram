bad99ae70336501a3f12a29713f77963fdb7d1c3
Roy-wonji <shuwj81@daum.net>
2022-04-28 15:33:30 +0900

    ♻️[refator]: feedController extension 파일  분리

--- instagram/instagram/Controllers/TabBarController/FeedController.swift
+++ instagram/instagram/Controllers/TabBarController/FeedController.swift
@@ -7,8 +7,6 @@
 
 import UIKit
 
-private let resueIdentifier = "Cell"
-
 final class FeedController:  UICollectionViewController {
     //MARK:  - Lifecycle
     override func viewDidLoad() {
@@ -18,27 +16,6 @@ final class FeedController:  UICollectionViewController {
     //MARK: - UI 설정 하는 함수
     func configureUI() {
         collectionView.backgroundColor = .white
-        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: resueIdentifier)
-    }
-}
-//MARK: - UICollectionViewDataSource
-extension FeedController {
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: resueIdentifier, for:  indexPath) as! FeedCell
-        return cell
-    }
-}
-//MARK:  - UICollectionViewDelegateFlowLayout
-extension FeedController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = view.frame.width
-        var height = width + 8 + 40 + 8
-        height += 50
-        height += 60
-        return CGSize(width: width, height: height)
+        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: CellIdentifier.resueIdentifier)
     }
 }
---
+++ instagram/instagram/VIew/Common/CellIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  CellIdentifier.swift
+//  instagram
+//
+//  Created by 서원지 on 2022/04/28.
+//
+
+import Foundation
+
+enum CellIdentifier {
+     static  let resueIdentifier = "Cell"
+}
