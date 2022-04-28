7e0b6e8fcfc59121662b6b520aef0835ad15128d
Roy-wonji <shuwj81@daum.net>
2022-04-29 01:50:33 +0900

    ✨[feat]: 오토레이아웃 제약  조건  추가  구현

--- instagram/instagram/VIew/UI/CustomTextField.swift
+++ instagram/instagram/VIew/UI/CustomTextField.swift
@@ -12,6 +12,11 @@ class CustomTextField: UITextField {
     init(placeholder: String) {
         super.init(frame: .zero)
         
+        let spacer  = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
         borderStyle = .none
         textColor = .white
         keyboardAppearance = .dark
