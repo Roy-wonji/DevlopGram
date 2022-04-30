4585adba4284cbc0dee6ffd35a429af495992e18
Roy-wonji <shuwj81@daum.net>
2022-04-30 23:28:16 +0900

    ✨[feat]:  RegistrationViewModel 의  프로로토콜 상속한  계산 속성 구현

--- instagram/instagram/View Model/RegistrationViewModel.swift
+++ instagram/instagram/View Model/RegistrationViewModel.swift
@@ -5,13 +5,24 @@
 //  Created by 서원지 on 2022/04/30.
 //
 
-import Foundation
+import UIKit
 
-struct RegistrationViewModel{
+struct RegistrationViewModel: AuthenticationViewModel {
     var email: String?
     var password: String?
     var fullName: String?
     var userName: String?
     
+    var formIsValid: Bool {
+        return email?.isEmpty == false  &&  password?.isEmpty == false
+        && fullName?.isEmpty == false  && userName?.isEmpty == false
+    }
+    
+    var buttonBackground: UIColor {
+        return formIsValid ? #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.5)
+    }
+    var buttonTitleColor: UIColor {
+        return  formIsValid ? .white  : UIColor(white: 1, alpha: 0.67)
+    }
     
 }
