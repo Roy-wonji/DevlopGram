e13900f04f6b9c0b4125d6c6b8822309507c687b
Roy-wonji <shuwj81@daum.net>
2022-04-30 21:48:51 +0900

    ✨[feat]: 사진 업로드 하는 버튼 구현

--- instagram/instagram/Controllers/Authentication/RegisterView/RegistrationController.swift
+++ instagram/instagram/Controllers/Authentication/RegisterView/RegistrationController.swift
@@ -6,17 +6,37 @@
 //
 
 import UIKit
+import Then
 
 final class RegistrationController: UIViewController {
     //MARK:  - Properties
+    
+    private lazy var plusPhotoButton = UIButton(type: .system).then{ button  in
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.tintColor = .white
+    }
+    
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
-
+    
+    //MARK: -  UI 관련
     private func configureUI() {
        configureGradientLayer()
+        setConstrants()
+    }
+    //MARK: 오토레이아웃 관련
+    private func setConstrants() {
+        setConstrantsPlusPhotoButton()
+    }
+    
+    private func setConstrantsPlusPhotoButton() {
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.centerX(inView: view)
+        plusPhotoButton.setDimensions(height: 140, width: 140)
+        plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
     }
 }
