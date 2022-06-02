15ff0673c7380e4b8e6f66c7b288856b5e40fdb7
Roy-wonji <shuwj81@daum.net>
2022-06-02 22:22:56 +0900

    ✨[feat]: HVUL-4  메세지 뷰 모델 구현 #3

---
+++ instagram/instagram/View Model/Comment/CommentViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  CommentViewmodel.swift
+//  instagram
+//
+//  Created by 서원지 on 2022/06/02.
+//
+
+import UIKit
+
+struct CommentViewModel {
+    
+    private let comment: Comment
+    
+    var profileImageUrl: URL? { return URL(string: comment.profileImageUrl) }
+
+    var username: String { return comment.username }
+    
+    var commentText: String { return comment.commentText }
+    
+    init(comment: Comment) {
+        self.comment = comment
+    }
+    
+    func  commentLabelText() -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: "\(username)", attributes: [.font: UIFont.boldSystemFont(ofSize: 14) ] )
+        
+        attributedString.append(NSAttributedString(string: commentText, attributes: [.font: UIFont.systemFont(ofSize: 14) ] ) )
+        return attributedString
+    }
+}
