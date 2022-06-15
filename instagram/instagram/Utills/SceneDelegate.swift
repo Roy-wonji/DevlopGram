2474cb9bea37982526ce775993d2d98792f5bba0
Roy-wonji <shuwj81@daum.net>
2022-06-15 22:55:39 +0900

    ✨[feat]:  HVUL-6 블러처리 구현 #9

--- instagram/instagram/Utills/SceneDelegate.swift
+++ instagram/instagram/Utills/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var imageView: UIImageView?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -27,18 +27,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        if let imageView = imageView {
+            imageView.removeFromSuperview()
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        guard let window = window else { return }
+        imageView  = UIImageView(frame: window.frame)
+        guard let imageView = imageView else {return}
+        imageView.image = UIImage(named: "instagramHome")
+        window.addSubview(imageView)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        self.imageView?.removeFromSuperview()
+        
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
