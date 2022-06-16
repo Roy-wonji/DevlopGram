ef4ef0f4edf8f3bd8fa02abe44e9fcb1b131a142
Roy-wonji <shuwj81@daum.net>
2022-06-16 22:18:47 +0900

    ✨[feat]:  HVUL-6 UNUserNotificationCenterDelegate 구현 #9

--- instagram/instagram/Utills/AppDelegate.swift
+++ instagram/instagram/Utills/AppDelegate.swift
@@ -12,28 +12,53 @@ import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         if let user = FirebaseAuth.Auth.auth().currentUser {
-                    print("로그인 되어 있음", user.uid, user.email ?? "-")
-                }
-                return true
+            print("로그인 되어 있음", user.uid, user.email ?? "-")
+        }
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { didAllow, error in
+            if didAllow {
+                print("권한 허가")
+            } else {
+                print("권한 요청 에러: \(error?.localizedDescription ?? "nil")")
+            }
+        }
+        UNUserNotificationCenter.current().delegate = self
+        
+        
+        return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if #available(iOS 14.0, *) {
+            completionHandler([.badge, .sound, .banner])
+        } else {
+            print("권한 거부 에러 버전을 확인 해주세요")
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let _  = response.notification.request.content.userInfo
+        completionHandler()
+    }
+}
